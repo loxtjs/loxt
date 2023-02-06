92aae40ee1227ff43d68efa699d13e3eb06b00d3
AngelNext <contact@angelnext.dev>
2023-02-06 21:55:34 +0100

    Fixed bugs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
 
 export default defineConfig({
+	site: 'https://loxt.angelnext.dev/',
 	integrations: [tailwind(), sitemap(), prefetch()],
 });
